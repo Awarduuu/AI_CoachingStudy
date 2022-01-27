3f825f22ee97d3d715c974ca7fc46304579c0a0f
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-01-27 17:44:08 +0900

    2주차 commit

---
+++ 1주차/calculate_rectangle.py
@@ -0,0 +1,15 @@
+def calculate_ractangle_area(x,y) :
+    """[summary]
+
+    Args:
+        x ([int]): [height of rectangle]
+        y ([int]): [width of rectangle]
+
+    Returns:
+        [int]: [area of rectangle]
+    """    
+    return x*y
+rectangle_x = 10
+rectangle_y = 20
+
+print(calculate_ractangle_area(rectangle_x,rectangle_y))
